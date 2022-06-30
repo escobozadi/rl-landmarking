e170db16f94cc23440168bb0c16a246bfd4f216e
escobozadi <dianaescoboza@gmail.com>
2022-06-30 16:29:55 -0400

    evaluate 2d

--- src/DQN.py
+++ src/DQN.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--file_type',
                 help='Type of the training and validation files',
-                choices=['brain', 'cardiac', 'fetal','joint'], default='train')
+                choices=['brain', 'cardiac', 'fetal', 'joint'], default='joint')
     parser.add_argument(
         '--files',
                 type=argparse.FileType('r'), nargs='+',
@@ -235,7 +235,7 @@ if __name__ == '__main__':
         dqn = DQN(agents, frame_history=FRAME_HISTORY, logger=logger,
                   type=args.model_name, collective_rewards=args.team_reward, attention=args.attention)
         model = dqn.q_network
-        model.load_state_dict(torch.load(args.load, map_location=model.device))
+        model.load_state_dict(torch.load(args.load, map_location=model.device))  # Load pre-trained model
         environment = get_player(files_list=args.files,
                                  file_type=args.file_type,
                                  landmark_ids=args.landmarks,
